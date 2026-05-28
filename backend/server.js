3258b4477185733eca9009d706ad79898677205b
Sania Khandakar <saniakhandakar2552@gmail.com>
2026-05-28 17:44:01 +0530

    Update CORS configuration to allow specific origin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const orderRoutes = require("./routes/orderRoutes");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+    origin: "https://ecommerce-website-lp52.vercel.app",
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
